cab5011ea543d767406a7e6a639bd2926c82a570
Lin Yang <coding@lyang.dev>
2026-02-22 09:07:53 -0800

    Publish snapshots to Maven Central (#55)
    Switches snapshot publishing from GitHub Packages to Maven Central
    using the central-publishing-maven-plugin's native SNAPSHOT support.
    
    - Append -SNAPSHOT to computed revision in release.sh
    - Move central-publishing-maven-plugin from profile to default plugins
    - Remove distributionManagement (GitHub Packages) and central profile
    - Update build.yaml to use Central credentials instead of GITHUB_TOKEN
    - Drop --activate-profiles central from release.yaml

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ SOURCE="${SOURCE:-$(git rev-parse --abbrev-ref HEAD)}"
 TIMESTAMP="$(git log -1 --pretty=%cd --date=format:'%Y%m%d%H%M%S')"
 COMMIT="$(git rev-parse --short HEAD)"
 MAJOR="${MAJOR:-0}"
-REVISION="${REVISION:-$MAJOR.$SOURCE.$TIMESTAMP.$COMMIT}"
+REVISION="${REVISION:-$MAJOR.$SOURCE.$TIMESTAMP.$COMMIT-SNAPSHOT}"
 
 mvn --define revision="$REVISION" --batch-mode "$@" clean deploy 1>&2
 

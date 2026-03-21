72b496a105db4e6513027fceef5d5f40f493c3ce
Lin Yang <coding@lyang.dev>
2026-03-20 18:55:06 -0700

    Add FieldSpec.of() static factory methods (#83)
    Simplify test FieldSpec definitions to use the new factory.

--- src/main/java/io/bytestreams/codec/core/FieldSpec.java
+++ src/main/java/io/bytestreams/codec/core/FieldSpec.java
@@ -1,6 +1,9 @@
 package io.bytestreams.codec.core;
 
 import io.bytestreams.codec.core.util.Predicates;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -27,5 +30,72 @@ public interface FieldSpec<T, V> {
    */
   default Predicate<T> presence() {
     return Predicates.alwaysTrue();
+  }
+
+  /**
+   * Creates a FieldSpec from explicit getter and setter functions.
+   *
+   * @param name the field name
+   * @param codec the codec for the field value
+   * @param getter function to extract the field value
+   * @param setter consumer to set the field value
+   * @param <T> the object type
+   * @param <V> the field value type
+   * @return a new FieldSpec
+   */
+  static <T, V> FieldSpec<T, V> of(
+      String name, Codec<V> codec, Function<T, V> getter, BiConsumer<T, V> setter) {
+    return of(name, codec, getter, setter, Predicates.alwaysTrue());
+  }
+
+  /**
+   * Creates a FieldSpec from explicit getter, setter, and presence predicate.
+   *
+   * @param name the field name
+   * @param codec the codec for the field value
+   * @param getter function to extract the field value
+   * @param setter consumer to set the field value
+   * @param presence predicate to determine if field is present
+   * @param <T> the object type
+   * @param <V> the field value type
+   * @return a new FieldSpec
+   */
+  static <T, V> FieldSpec<T, V> of(
+      String name,
+      Codec<V> codec,
+      Function<T, V> getter,
+      BiConsumer<T, V> setter,
+      Predicate<T> presence) {
+    Objects.requireNonNull(name, "name");
+    Objects.requireNonNull(codec, "codec");
+    Objects.requireNonNull(getter, "getter");
+    Objects.requireNonNull(setter, "setter");
+    Objects.requireNonNull(presence, "presence");
+    return new FieldSpec<>() {
+      @Override
+      public String name() {
+        return name;
+      }
+
+      @Override
+      public Codec<V> codec() {
+        return codec;
+      }
+
+      @Override
+      public V get(T object) {
+        return getter.apply(object);
+      }
+
+      @Override
+      public void set(T object, V value) {
+        setter.accept(object, value);
+      }
+
+      @Override
+      public Predicate<T> presence() {
+        return presence;
+      }
+    };
   }
 }
--- src/test/java/io/bytestreams/codec/core/FieldSpecTest.java
+++ src/test/java/io/bytestreams/codec/core/FieldSpecTest.java
@@ -1,6 +1,7 @@
 package io.bytestreams.codec.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,27 +20,7 @@ class FieldSpecTest {
   }
 
   static final FieldSpec<TestObject, Integer> ID =
-      new FieldSpec<>() {
-        @Override
-        public String name() {
-          return "id";
-        }
-
-        @Override
-        public Codec<Integer> codec() {
-          return Codecs.uint8();
-        }
-
-        @Override
-        public Integer get(TestObject object) {
-          return object.getId();
-        }
-
-        @Override
-        public void set(TestObject object, Integer value) {
-          object.setId(value);
-        }
-      };
+      FieldSpec.of("id", Codecs.uint8(), TestObject::getId, TestObject::setId);
 
   @Test
   void name() {
@@ -53,7 +34,29 @@ class FieldSpecTest {
 
   @Test
   void presence_defaults_to_always_true() {
-    assertThat(ID.presence().test(new TestObject())).isTrue();
+    FieldSpec<TestObject, Integer> spec =
+        new FieldSpec<>() {
+          @Override
+          public String name() {
+            return "id";
+          }
+
+          @Override
+          public Codec<Integer> codec() {
+            return Codecs.uint8();
+          }
+
+          @Override
+          public Integer get(TestObject object) {
+            return object.getId();
+          }
+
+          @Override
+          public void set(TestObject object, Integer value) {
+            object.setId(value);
+          }
+        };
+    assertThat(spec.presence().test(new TestObject())).isTrue();
   }
 
   @Test
@@ -61,5 +64,82 @@ class FieldSpecTest {
     TestObject obj = new TestObject();
     ID.set(obj, 42);
     assertThat(ID.get(obj)).isEqualTo(42);
+  }
+
+  // -- FieldSpec.of() factory --
+
+  static final FieldSpec<TestObject, Integer> ID_OF =
+      FieldSpec.of("id", Codecs.uint8(), TestObject::getId, TestObject::setId);
+
+  @Test
+  void of_name() {
+    assertThat(ID_OF.name()).isEqualTo("id");
+  }
+
+  @Test
+  void of_codec() {
+    assertThat(ID_OF.codec()).isNotNull();
+  }
+
+  @Test
+  void of_get_and_set() {
+    TestObject obj = new TestObject();
+    ID_OF.set(obj, 42);
+    assertThat(ID_OF.get(obj)).isEqualTo(42);
+  }
+
+  @Test
+  void of_presence_defaults_to_always_true() {
+    assertThat(ID_OF.presence().test(new TestObject())).isTrue();
+  }
+
+  @Test
+  void of_with_presence() {
+    FieldSpec<TestObject, Integer> spec =
+        FieldSpec.of(
+            "id", Codecs.uint8(), TestObject::getId, TestObject::setId, obj -> obj.getId() > 0);
+    TestObject obj = new TestObject();
+    assertThat(spec.presence().test(obj)).isFalse();
+    obj.setId(1);
+    assertThat(spec.presence().test(obj)).isTrue();
+  }
+
+  @Test
+  void of_null_name_rejected() {
+    Codec<Integer> codec = Codecs.uint8();
+    assertThatThrownBy(() -> FieldSpec.of(null, codec, TestObject::getId, TestObject::setId))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessageContaining("name");
+  }
+
+  @Test
+  void of_null_codec_rejected() {
+    assertThatThrownBy(() -> FieldSpec.of("id", null, TestObject::getId, TestObject::setId))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessageContaining("codec");
+  }
+
+  @Test
+  void of_null_getter_rejected() {
+    Codec<Integer> codec = Codecs.uint8();
+    assertThatThrownBy(() -> FieldSpec.of("id", codec, null, TestObject::setId))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessageContaining("getter");
+  }
+
+  @Test
+  void of_null_setter_rejected() {
+    Codec<Integer> codec = Codecs.uint8();
+    assertThatThrownBy(() -> FieldSpec.of("id", codec, TestObject::getId, null))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessageContaining("setter");
+  }
+
+  @Test
+  void of_null_presence_rejected() {
+    Codec<Integer> codec = Codecs.uint8();
+    assertThatThrownBy(() -> FieldSpec.of("id", codec, TestObject::getId, TestObject::setId, null))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessageContaining("presence");
   }
 }
